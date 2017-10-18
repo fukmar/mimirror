8d9dcc821f5780c713d44b15b56b7fb3a063f895
Franco <francopanario@hotmail.com>
2017-10-18 15:38:41 -0300

    agrego columns names en anotation

--- TPO_AD2017_Servidor/src/entities/CartaEntity.java
+++ TPO_AD2017_Servidor/src/entities/CartaEntity.java
@@ -11,6 +11,7 @@ import enumns.Temporada;
 @Table(name="cartas")
 public class CartaEntity 
 {
+	@Column(name="codCarta")
 	private Integer codigoCarta;
 	private Date vigencia;
 	private Temporada temporada;
