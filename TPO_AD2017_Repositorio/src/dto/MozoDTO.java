682411b1e731849e0b18fa0347a50bca49e97329
Franco <francopanario@hotmail.com>
2017-10-23 19:14:27 -0300

    -agrego cuerpo(codigo) a DTOs ya declarados

--- TPO_AD2017_Repositorio/src/dto/MozoDTO.java
+++ TPO_AD2017_Repositorio/src/dto/MozoDTO.java
@@ -8,5 +8,57 @@ public class MozoDTO implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1999050924715622562L;
+	
+	
+	private Integer dni;
+	private String nombre, apellido;
+	private Float procentajeComision;
+	
+	public MozoDTO(){}
+	
+	public MozoDTO(Integer dni, String nombre, String apellido, Float procentajeComision) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.procentajeComision = procentajeComision;
+	
+	}
 
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Float getProcentajeComision() {
+		return procentajeComision;
+	}
+
+	public void setProcentajeComision(Float procentajeComision) {
+		this.procentajeComision = procentajeComision;
+	}
+
+
+	
+	
+	
 }
--- TPO_AD2017_Repositorio/src/dto/SolicitudInsumoDTO.java
+++ TPO_AD2017_Repositorio/src/dto/SolicitudInsumoDTO.java
@@ -1,6 +1,8 @@
 package dto;
 
 import java.io.Serializable;
+import java.util.Date;
+
 
 public abstract class SolicitudInsumoDTO implements Serializable
 {
@@ -9,5 +11,99 @@ public abstract class SolicitudInsumoDTO implements Serializable
 	 * 
 	 */
 	private static final long serialVersionUID = -8544151372636600303L;
+	
+	
+	protected Integer codigoArt;
+	protected Integer cantidad;
+	protected AreaRestaurantDTO area;
+	protected String responsable;
+	protected Integer lote;
+	protected Date fechaCompra;
+	protected Date fechaVencimiento;
+	protected String motivo;
+	
+	public SolicitudInsumoDTO(){
+		
+	}
+
+	public SolicitudInsumoDTO(Integer codigoArt, Integer cantidad,
+			AreaRestaurantDTO area, String responsable, Integer lote,
+			Date fechaCompra, Date fechaVencimiento, String motivo) {
+		super();
+		this.codigoArt = codigoArt;
+		this.cantidad = cantidad;
+		this.area = area;
+		this.responsable = responsable;
+		this.lote = lote;
+		this.fechaCompra = fechaCompra;
+		this.fechaVencimiento = fechaVencimiento;
+		this.motivo = motivo;
+	}
+
+	public Integer getCodigoArt() {
+		return codigoArt;
+	}
+
+	public void setCodigoArt(Integer codigoArt) {
+		this.codigoArt = codigoArt;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public AreaRestaurantDTO getArea() {
+		return area;
+	}
+
+	public void setArea(AreaRestaurantDTO area) {
+		this.area = area;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(String responsable) {
+		this.responsable = responsable;
+	}
+
+	public Integer getLote() {
+		return lote;
+	}
+
+	public void setLote(Integer lote) {
+		this.lote = lote;
+	}
+
+	public Date getFechaCompra() {
+		return fechaCompra;
+	}
+
+	public void setFechaCompra(Date fechaCompra) {
+		this.fechaCompra = fechaCompra;
+	}
+
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+	
+	
 	
 }
