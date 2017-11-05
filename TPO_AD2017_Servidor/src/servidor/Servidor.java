e66276ee779437d92ca127cc1272587b802c55fa
netneko <cecizucchino@gmail.com>
2017-11-05 18:37:54 -0300

    Limpie la clase SERVER. La clase SERVER no extiende de nada,NO implementa SERIALIZABLE ni nada mas. Las pruebas NO LAS HAGAN en el server por favor

--- TPO_AD2017_Servidor/src/servidor/Servidor.java
+++ TPO_AD2017_Servidor/src/servidor/Servidor.java
@@ -21,108 +21,28 @@ import remoto.ObjetoRemoto;
 
 public class Servidor {
 
-	protected Servidor() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
+	public void main (String args[])
+	{
+		new Servidor();
 	}
-
 	
-	public static void main(String[] args) throws MalformedURLException {
-	 ObjetoRemoto remoteObject;
-	try {
-		remoteObject = new ObjetoRemoto();
-		LocateRegistry.createRegistry(1099);
-		Naming.rebind("//localhost/manejoNegocio", remoteObject);
-	} catch (RemoteException e) {
-		// TODO Auto-generated catch block
-	
+	public Servidor()
+	{
+		try {
+			LocateRegistry.createRegistry(1099);
+			manejoNegocio objetoRemoto=new ObjetoRemoto();
+			Naming.rebind("//localhost/ObjetoRemoto",objetoRemoto);
+			System.out.println("Fijado en //localhost/ObjetoRemoto ");
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
-		System.out.print("OK");
 		
-		//aca van pruebas de DB
-	/*	LocalEntity test = new LocalEntity(1,"aca","alla");
-		SessionFactory sf = hbt.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.save(test);
-		session.getTransaction().commit();
-		session.close();
-		*/
-		/*
-		Estado est = null;
-		PlanDeProduccionEntity test = new PlanDeProduccionEntity(est.EnProceso);
-		SessionFactory sf = hbt.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.save(test);
-		session.getTransaction().commit();
-		session.close();
-		*/
-		/*
-		UnidadEntity test = new UnidadEntity("unidad uno");
-		SessionFactory sf = hbt.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.save(test);
-		session.getTransaction().commit();
-		session.close();
-		*/
-		/*
-		MozoEntity test = new MozoEntity(31575032,"Nahuelito","Grisoluble",80.4f);
-		SessionFactory sf = hbt.getSessionFactory();
-		Session session = sf.openSession();
-		session.beginTransaction();
-		session.save(test);
-		session.getTransaction().commit();
-		session.close();
-		*/
-		
-		
-		UnidadEntity ue = new UnidadEntity("gramos");
-	
-		Estado est = null;
-		PlanDeProduccionEntity pdpe = new PlanDeProduccionEntity(est.EnProceso);
-		Date fecha = new Date("10/10/2020");
-		
-		MateriaPrimaEntity mpe = new MateriaPrimaEntity("desc",ue);
-	
-		List<MateriaPrimaEntity> materiales = new ArrayList<MateriaPrimaEntity>();
-		materiales.add(mpe);
-		SemiElaboradoEntity see = new SemiElaboradoEntity("tipo","calidad","desc",pdpe,1,fecha,materiales,ue);
-		
-		List<SemiElaboradoEntity> componentes = new ArrayList<SemiElaboradoEntity>();
-		componentes.add(see);
-		
-		ElaboradoEntity ee = new ElaboradoEntity("tipo","calidad","pizza",pdpe,1,fecha, ue, componentes);
-			
-		
-		List<ElaboradoEntity> elabs = new ArrayList<ElaboradoEntity>();
-		elabs.add(ee);
-		
-		PlatoEntity test = new PlatoEntity("platito",13f,elabs);
-
-		SessionFactory sf = hbt.getSessionFactory();
-		Session session = sf.openSession();
-		
-		session.beginTransaction();
-		session.save(test);
-		session.getTransaction().commit();
-		session.close();
-		
-		//aca terminan pruebas de DB
-		
-		
-		
-}
-
-	
-
-/*
- 	public List<DTO> funcion() throws RemoteException {
-		// TODO Auto-generated method stub
-		return manejoNegocio.getInstance().getUnidades();
 	}
- 
- */
+		
+
+
 }
--- TPO_AD2017_Servidor/src/test/testHibernate.java
+++ TPO_AD2017_Servidor/src/test/testHibernate.java
@@ -1,5 +1,19 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import entities.ElaboradoEntity;
+import entities.MateriaPrimaEntity;
+import entities.PlanDeProduccionEntity;
+import entities.PlatoEntity;
+import entities.SemiElaboradoEntity;
+import entities.UnidadEntity;
+import enumns.Estado;
 import hibernate.hbt;
 
 public class testHibernate {
@@ -9,6 +23,89 @@ public class testHibernate {
 	{
 		
 		hbt.getSessionFactory();
+		
+		/*
+		 * 	//aca van pruebas de DB
+	/*	LocalEntity test = new LocalEntity(1,"aca","alla");
+		SessionFactory sf = hbt.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(test);
+		session.getTransaction().commit();
+		session.close();
+		*/
+		/*
+		Estado est = null;
+		PlanDeProduccionEntity test = new PlanDeProduccionEntity(est.EnProceso);
+		SessionFactory sf = hbt.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(test);
+		session.getTransaction().commit();
+		session.close();
+		*/
+		/*
+		UnidadEntity test = new UnidadEntity("unidad uno");
+		SessionFactory sf = hbt.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(test);
+		session.getTransaction().commit();
+		session.close();
+		*/
+		/*
+		MozoEntity test = new MozoEntity(31575032,"Nahuelito","Grisoluble",80.4f);
+		SessionFactory sf = hbt.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(test);
+		session.getTransaction().commit();
+		session.close();
+		
+		
+		
+		UnidadEntity ue = new UnidadEntity("gramos");
+	
+		Estado est = null;
+		PlanDeProduccionEntity pdpe = new PlanDeProduccionEntity(est.EnProceso);
+		Date fecha = new Date("10/10/2020");
+		
+		MateriaPrimaEntity mpe = new MateriaPrimaEntity("desc",ue);
+	
+		List<MateriaPrimaEntity> materiales = new ArrayList<MateriaPrimaEntity>();
+		materiales.add(mpe);
+		SemiElaboradoEntity see = new SemiElaboradoEntity("tipo","calidad","desc",pdpe,1,fecha,materiales,ue);
+		
+		List<SemiElaboradoEntity> componentes = new ArrayList<SemiElaboradoEntity>();
+		componentes.add(see);
+		
+		ElaboradoEntity ee = new ElaboradoEntity("tipo","calidad","pizza",pdpe,1,fecha, ue, componentes);
+			
+		
+		List<ElaboradoEntity> elabs = new ArrayList<ElaboradoEntity>();
+		elabs.add(ee);
+		
+		PlatoEntity test = new PlatoEntity("platito",13f,elabs);
+
+		SessionFactory sf = hbt.getSessionFactory();
+		Session session = sf.openSession();
+		
+		session.beginTransaction();
+		session.save(test);
+		session.getTransaction().commit();
+		session.close();
+		
+		//aca terminan pruebas de DB
+		 * 
+		 * /*
+ 	public List<DTO> funcion() throws RemoteException {
+		// TODO Auto-generated method stub
+		return manejoNegocio.getInstance().getUnidades();
+	}
+ 
+ */
+		
+		 **/
 	}
 
 }
