95a4c09974ef130ba7d2effb5704cf5bbc158ba5
netneko <cecizucchino@gmail.com>
2017-11-05 18:27:00 -0300

    Clase TEST hibernate creada para que hagan todas sus purebas ahi y no ensucien el SERVER

---
+++ TPO_AD2017_Servidor/src/test/testHibernate.java
@@ -0,0 +1,14 @@
+package test;
+
+import hibernate.hbt;
+
+public class testHibernate {
+
+	//Hagan sus pruebas relacionadas a HIBERNATE ACA
+	public static void main(String[] args) 
+	{
+		
+		hbt.getSessionFactory();
+	}
+
+}
