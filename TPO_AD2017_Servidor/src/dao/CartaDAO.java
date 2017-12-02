f68cb99445a1c098a67a115a3b03ecd02ba1e2ff
FlaviuS <FlaviuS@DESKTOP-MFPTO42>
2017-12-02 13:25:49 -0300

    CartaDAO

---
+++ TPO_AD2017_Servidor/src/dao/CartaDAO.java
@@ -0,0 +1,55 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import entities.CartaEntity;
+import entities.MesaEntity;
+import hibernate.HibernateUtil;
+import negocio.Carta;
+import negocio.Mesa;
+
+public class CartaDAO {
+
+	private static CartaDAO instancia;
+	private static SessionFactory sf=null;
+		
+		private CartaDAO(){}
+		
+		public static CartaDAO getInstance()
+		{
+			if(instancia == null) {
+				instancia = new CartaDAO();
+				sf=HibernateUtil.getSessionFactory();
+			}
+			return instancia;
+		}
+		
+
+		public Carta obtenerCartaPorCod(int nrocarta)
+		{
+			Carta carta=new Carta();
+			SessionFactory sf = HibernateUtil.getSessionFactory();
+			Session session = sf.openSession();
+			//MesaEntity mesa=new MesaEntity();
+			CartaEntity resu= (CartaEntity)session.createQuery("FROM CartaEntity c WHERE c.codigoCarta=?").setInteger(0,nrocarta).setFirstResult(0).setMaxResults(1).uniqueResult();
+			carta=resu.toNegocio();
+			return carta;
+		}
+		
+		public List<Carta> getCartas()
+		{
+			Session session=sf.openSession();
+			List<Carta> listaM=new ArrayList<Carta>();
+			List<CartaEntity> resu=session.createQuery("from CartaEntity c").list();
+			for(CartaEntity c:resu)
+			{
+				listaM.add(c.toNegocio());
+			}
+			session.close();
+			return listaM;
+		}  
+}
